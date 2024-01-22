54c2513c26e7cfb3ee61728d803e264149e0384f
Meri11Ra1 <r.t.00293794@gmail.com>
2024-01-23 07:40:15 +0900

    clean: extra importing in second_screen

--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'first_screen.dart';
+
 
 class SecondScreen extends StatelessWidget {
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
